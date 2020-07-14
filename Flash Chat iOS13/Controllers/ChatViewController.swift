f2f84ca53aa8a82090b6afedd7b142daf90ff777
Ramzi Chamcham <ramzichamcham@gmail.com>
2020-07-14 16:48:52 -0400

    commit

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -7,19 +7,123 @@
 //
 
 import UIKit
-
+import Firebase
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        title = K.appName
+        navigationItem.hidesBackButton = true
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        loadMessages()
+        
+    }
+    
+    func loadMessages(){
+        db.collection(K.FStore.collectionName)
+            .order(by: K.FStore.dateField)
+            .addSnapshotListener { (querrySnapshot, error) in
+                
+                self.messages = []
+                
+                if let error = error{
+                    print("Error getting documents \(error)")
+                }else{
+                    if let snapshotDocuments = querrySnapshot?.documents{
+                        for doc in snapshotDocuments{
+                            let data = doc.data()
+                            if let messageSender = data[K.FStore.senderField] as? String, let messageBody = data[K.FStore.bodyField] as? String {
+                                let newMessage = Message(sender: messageSender, body: messageBody)
+                                self.messages.append(newMessage)
+                                /*we have to call the following method because by the time we get a response and this closure is executed the tableview would have already been filled with the messages array empty.
+                                 it is wrapped in a DispatchQueue.main.async block because otherwise we are updating the UI from a closure (so in the background) and not from the main thread (the process happening in the foreground) which is how it should be done
+                                 DispatchQueue.main.async gets a hold of the main thread.
+                                 */
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
+                                    let indexPath = IndexPath(row: self.messages.count - 1, section: 0)
+                                    self.tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                                }
+                            }
+                        }
+                    }
+                }
+        }
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data:
+                [K.FStore.senderField: messageSender,
+                 K.FStore.bodyField: messageBody,
+                 K.FStore.dateField: Date().timeIntervalSince1970
+            ]){ (error) in
+                if let e = error{
+                    print("there was an issue saving data to firestore: \(e)")
+                }else{
+                    print("data saved successfully")
+                    DispatchQueue.main.async {
+                        self.messageTextfield.text = ""  
+                    }
+                }
+            }
+        }
+        
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+    }
+    
 }
+
+extension ChatViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Fetch a cell of the appropriate type.
+        let message = messages[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        
+        // Configure the cell’s contents.
+        cell.label.text = message.body
+        
+        //this is a message from the current user
+        if message.sender == Auth.auth().currentUser?.email{
+            cell.leftImageView.isHidden = true
+            cell.rightImageView.isHidden = false
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.lightPurple)
+            cell.label.textColor = UIColor(named: K.BrandColors.purple)
+        }
+            //this is a message from another sender
+        else{
+            cell.leftImageView.isHidden = false
+            cell.rightImageView.isHidden = true
+            cell.messageBubble.backgroundColor = UIColor(named: K.BrandColors.purple)
+            cell.label.textColor = UIColor(named: K.BrandColors.lightPurple)
+        }
+        
+        
+        return cell
+    }
+    
+}
+
